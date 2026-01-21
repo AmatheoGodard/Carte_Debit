ceae3ba67567586d4b55a6c709ec1115a44bfa17
AmatheoGodard <godarda@saintjo.org>
2026-01-21 11:57:38 +0100

    Initial commit

--- src/pj_cartedebit/Model/M_Users.java
+++ src/pj_cartedebit/Model/M_Users.java
@@ -16,7 +16,7 @@ import pj_cartedebit.Db_mariadb;
  * @author amatheo
  */
 public class M_Users {
-    
+
     private Db_mariadb db;
     private int id_user, id_roles;
     private String name, email, password, remember_token, commentaire;
@@ -47,9 +47,9 @@ public class M_Users {
         this.email_verified_at = email_verified_at;
         this.created_at = created_at;
         this.updated_at = updated_at;
-        
+
         String sql = "INSERT INTO mcd_users (name, email, email_verified_at, password, remember_token, commentaire, created_at, updated_at, id_role)"
-                        + " VALUES ('" + name + "', '" + email + "', '" + password + "', '" + remember_token + "', '" + commentaire + "', '" + email_verified_at + "', '" + created_at +"','" + updated_at +"','" + id_roles +"');";
+                + " VALUES ('" + name + "', '" + email + "', '" + password + "', '" + remember_token + "', '" + commentaire + "', '" + email_verified_at + "', '" + created_at + "','" + updated_at + "','" + id_roles + "');";
         db.sqlExec(sql);
         ResultSet res;
         res = db.sqlLastId();
@@ -61,7 +61,7 @@ public class M_Users {
     public M_Users(Db_mariadb db, int id_user) throws SQLException {
         this.db = db;
         this.id_user = id_user;
-        
+
         String sql = "SELECT * FROM mcd_users WHERE id = '" + id_user + "'";
         ResultSet res;
         res = db.sqlSelect(sql);
@@ -77,6 +77,112 @@ public class M_Users {
         this.id_roles = res.getInt("id_role");
         res.close();
     }
+
+    public Db_mariadb getDb() {
+        return db;
+    }
+
+    public void setDb(Db_mariadb db) {
+        this.db = db;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public int getId_roles() {
+        return id_roles;
+    }
+
+    public void setId_roles(int id_roles) {
+        this.id_roles = id_roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRemember_token() {
+        return remember_token;
+    }
+
+    public void setRemember_token(String remember_token) {
+        this.remember_token = remember_token;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public LocalDateTime getEmail_verified_at() {
+        return email_verified_at;
+    }
+
+    public void setEmail_verified_at(LocalDateTime email_verified_at) {
+        this.email_verified_at = email_verified_at;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public void update() throws SQLException {
+        String sql;
+        sql = "UPDATE mcd_users SET "
+                + "name='" + name + "', "
+                + "email='" + email + "', "
+                + "email_verified_at='" + email_verified_at + "', "
+                + "password='" + password + "', "
+                + "remember_token='" + remember_token + "', "
+                + "commentaire='" + commentaire +"', "
+                + "created_at='" + created_at + "', "
+                + "updated_at='" + updated_at + "', "
+                + "id_role='" + id_roles + "' "
+                + "WHERE id=" + id_user + ";";
+
+        db.sqlExec(sql);
+    }
     
-    
+    public void delete() throws SQLException {
+        String sql;
+        sql = "DELETE FROM mcd_users WHERE id=" + id_user + ";";
+        db.sqlExec(sql);
+    }
+
 }
